92efbf744c7c2d7ee5f38c184cff5957834ffdf3
Elijah_A <33457679+Casual994@users.noreply.github.com>
2018-08-31 13:21:02 +0200

    uppgift 1, okänttal

--- src/uppgiftergithub/vecka35.java
+++ src/uppgiftergithub/vecka35.java
@@ -1,5 +1,26 @@
 package uppgiftergithub;
 
 public class vecka35 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(okänttal(1,2,3,4,123,123,123));
+		
+	}
 
+	public static int okänttal(int... n) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < n.length; i++) {
+			
+			sum = sum + n[i];
+		}
+		
+		
+		return sum;
+		
+	}
+	
+	
 }
